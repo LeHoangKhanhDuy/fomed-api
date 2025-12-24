65139b78cba4ccfe3cd6f2f158246bea7d6c55d5
Lê Hoàng Khánh Duy <lhkhanhduy.dev@gmail.com>
2025-12-24 13:30:31 +0700

    feat(dep): add .zip

--- obj/Debug/net9.0/FoMed_WebAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/FoMed_WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoMed_WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e77e122e82c18c79e307353e17ef90b49ab590")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3565816d7457027099a1b6fcbc902d462d25aad")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoMed_WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoMed_WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
